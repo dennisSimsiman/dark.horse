68c55c6907ab6203b09f4388a49e283c8d744ef0
kyrja <christyeicher@yahoo.com>
2016-03-28 14:32:42 -0700

    Built skeleton of index.php and made it look purty (at least I think so!). Added the headers and divs in which to later place the images.
    TODO: Take the doc-level styles in index.php, transfer them to /src/styles/style.css, and include the css file. (I was having problems including the external stylesheet, so I skipped over it temporarily for the sake of time/productivity)

---
+++ index.php
@@ -0,0 +1,66 @@
+<html>
+<head>
+    <title>Image Rater</title>
+    <style>
+        body {
+            background-color: darkslategray;
+        }
+        div#wrapper {
+            border-radius: 25px;
+            background: #73aD21;
+            padding: 20px;
+            margin-left: 15%;
+            margin-right: 15%;
+        }
+        div#header-links {
+            float: right;
+            font-size: medium;
+        }
+        a {
+            color: white;
+            cursor: crosshair;
+        }
+        a:hover{
+            color: black;
+        }
+        h1 {
+            text-align: center;
+            font-size: x-large;
+        }
+        h2 {
+            font-size: large;
+        }
+        div.wrapper-box{
+            border-radius: 25px;
+            background: #2F4F4F;
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div id="wrapper">
+
+        <h1>Image Rater [logo]</h1>
+
+        <div id="header-links">
+            <a href="">SIGN IN</a>
+             |
+            <a href="">SIGN UP</a>
+        </div>
+
+
+        <h2>Recent Images</h2>
+
+        <div id="recent-images" class="wrapper-box">
+
+        </div>
+
+
+        <h2>Popular Images</h2>
+
+        <div id="popular-images" class="wrapper-box">
+
+        </div>
+    </div>
+</body>
+</html>
