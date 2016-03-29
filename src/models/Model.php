926ffd104dc6b37770446bfab284ad41f8bbf22c
kyrja <christyeicher@yahoo.com>
2016-03-29 14:43:13 -0700

    Moved what was in Model.php over to Controller.php. The controller is what handles form data, not the model :B

--- src/models/Model.php
+++ src/models/Model.php
@@ -1,40 +1,7 @@
 <?php
 
-// Display any errors
-ini_set('display_errors',1);
-error_reporting(E_ALL);
-$goodToGo = 1;
+namespace dark_horse\hw3\models;
 
-// Saving the path
-$uploaddir = '../resources/';
-$uploadfile = $uploaddir . basename($_FILES['photo']['name']);
-$imageFileType = pathinfo($uploadfile,PATHINFO_EXTENSION);
 
-// Check if file already exists
-if (file_exists($uploadfile)) {
-    echo "File already exists! \n";
-    $goodToGo = 0;
-}
-// Check file size
-if ($_FILES["photo"]["size"] > 500000) {
-    echo "Too big! \n";
-    $goodToGo = 0;
-}
-// Only allow jpeg, jpg, and png
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-    echo "Sorry, only JPG, JPEG, & PNG files are allowed. \n";
-    $goodToGo = 0;
-}
-
-// If all is good, complete the upload.
-if ($goodToGo == 1) {
-    if (move_uploaded_file($_FILES['photo']['tmp_name'], "$uploadfile")) {
-        echo "File uploaded!\n";
-    } else {
-        echo "There was an error uploading your file :( Maybe this will help: \n";
-        print_r($_FILES);
-    }
-}
-else echo "Whoops! There was a problem uploading your image.";
 
 ?>
--- src/views/elements/Element.php
+++ src/views/elements/Element.php
@@ -7,7 +7,7 @@ namespace dark_horse\hw3\views\elements;
 <html>
 <body>
 
-<form action = "../../models/Model.php" method = "post" enctype = "multipart/form-data">
+<form action = "../../controllers/Controller.php" method = "post" enctype = "multipart/form-data">
 	Upload a photo! <input type = "file" name = "photo" size = "25" />
 	<input type = "submit" name = "submit" value = "Submit" />
 </form>
